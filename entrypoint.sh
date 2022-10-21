a51efa16fce11d4059a2256fc839b01f64a5bbf3
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2022-10-21 16:01:49 +0100

    exit when no outdated dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 chmod +x gradlew
 ./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json
 outdated=$(jq -r '.outdated.dependencies | .[] | "\(.name) \(.version) -> \(.available.release)"' < build/dependencyUpdates/report.json)
+if [ -z "$outdated" ] ; then
+  echo "Congratulations, all your dependencies have the latest releases!"
+  exit 1
+fi
 echo "outdated<<EOF" >> $GITHUB_ENV
 echo "$outdated" >> $GITHUB_ENV
 echo "EOF" >> $GITHUB_ENV
