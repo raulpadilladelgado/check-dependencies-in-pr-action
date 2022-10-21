63c427453c66d32ba8a862075c86ee821936d647
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2022-10-21 15:37:56 +0100

    use github environment variables instead of deprecated method

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 chmod +x gradlew
 ./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json
 outdated=$(jq -r '.outdated.dependencies | .[] | "\(.name) \(.version) -> \(.available.release)"' < build/dependencyUpdates/report.json)
-echo "outdated=$outdated" >>$GITHUB_OUTPUT
+echo "outdated<<EOF" >> $GITHUB_ENV
+echo "$outdated" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
