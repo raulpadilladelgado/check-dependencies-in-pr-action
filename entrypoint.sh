e0736bc86eae8528f3cd7bcd23eaf5cf078758ab
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2022-10-23 22:21:32 +0100

    add support for colors in the message

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,13 @@ if [ -z "$outdated" ]; then
   exit 0
 fi
 
-outdated_formatted=$(echo "**Dependencies with newer available releases:**
-```diff
-$outdated
-```
-")
-
 curl \
   -X POST \
   "$1" \
   -H "Content-Type: application/json" \
   -H "Authorization: token $2" \
-  --data '{ "body": '"\"$outdated_formatted\""' }'
+  --data '{ "body": '"\"Dependencies with newer available releases:
+  \```diff
+  $outdated
+  \```
+  \""' }'
