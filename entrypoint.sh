d82b081fa562e61a07c8d851855f8cc31d6fb6ab
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2022-10-23 22:12:16 +0100

    add support for colors in the message

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,21 @@
 
 chmod +x gradlew
 ./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json
-outdated=$(jq -r '.outdated.dependencies | .[] | "\(.name) \(.version) -> \(.available.release)"' <build/dependencyUpdates/report.json)
+outdated=$(jq -r '.outdated.dependencies | .[] | "📦 \(.name)","- \(.version)","+ \(.available.release)"' build/dependencyUpdates/report.json)
 if [ -z "$outdated" ]; then
   echo "Congratulations, all your dependencies have the latest releases!"
   exit 0
 fi
 
-echo "$1"
-echo "$2"
-echo "$outdated"
+outdated_formatted=$(echo "**Dependencies with newer available releases:**
+```diff
+$outdated
+```
+")
 
 curl \
   -X POST \
   "$1" \
   -H "Content-Type: application/json" \
   -H "Authorization: token $2" \
-  --data '{ "body": '"\"$outdated\""' }'
+  --data '{ "body": '"\"$outdated_formatted\""' }'
