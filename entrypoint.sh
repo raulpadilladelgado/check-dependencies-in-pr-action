fc6af46664a89da9210a41d96903df2450a28d09
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2022-10-27 23:25:11 +0100

    inline format json

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 chmod +x gradlew
 ./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json
-outdated=$(jq -r '.outdated.dependencies | .[] | "\(.name)\\\\n- \(.version)\\\\n+\(.available.release)\\\\n"' build/dependencyUpdates/report.json)
+outdated=$(jq -r '.outdated.dependencies | .[] | "\(.name)\\\\n-\(.version)\\\\n+\(.available.release)\\\\n"' build/dependencyUpdates/report.json)
 if [ -z "$outdated" ]; then
   echo "Congratulations, all your dependencies have the latest releases!"
   exit 0
@@ -17,5 +17,17 @@ curl \
   "
   {
         \"body\": \"**Dependencies with newer available releases:**\n\`\`\`diff\n$outdated\`\`\`\"
+  }
+  "
+
+curl \
+  -X POST \
+  "https://api.github.com/repos/raulpadilladelgado/aguacate/issues/2/comments" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: token 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" \
+  -d \
+  "
+  {
+        \"body\": \"**Dependencies with newer available releases:**\n\`\`\`diff\naguacate\n-1.2\n+2.3\n\`\`\`\"
   }
   "
