f3b8bac4ab0f7e5f08a16b479156d9d1cd702d88
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2022-10-28 15:07:24 +0100

    inline format json

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,9 @@
 
 chmod +x gradlew
 ./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json
-outdated=$(jq -r '.outdated.dependencies | .[] | "\(.name)\\\\n-\(.version)\\\\n+\(.available.release)\\\\n"' build/dependencyUpdates/report.json)
+outdated=$(jq -r '.outdated.dependencies | .[] | "\(.name) \(.version) => \(.available.release)"' build/dependencyUpdates/report.json)
 if [ -z "$outdated" ]; then
-  echo "Congratulations, all your dependencies have the latest releases!"
-  exit 0
+  outdated="Congratulations, all your dependencies have the latest releases!"
 fi
 
 curl \
@@ -16,18 +15,6 @@ curl \
   -d \
   "
   {
-        \"body\": \"**Dependencies with newer available releases:**\n\`\`\`diff\n"$outdated"\n\`\`\`\"
+        \"body\": \"$outdated\"
   }
   "
-
-#curl \
-#  -X POST \
-#  "https://api.github.com/repos/raulpadilladelgado/aguacate/issues/2/comments" \
-#  -H "Content-Type: application/json" \
-#  -H "Authorization: token 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" \
-#  -d \
-#  "
-#  {
-#        \"body\": \"**Dependencies with newer available releases:**\n\`\`\`diff\naguacate\n-1.2\n+2.3\n\`\`\`\"
-#  }
-#  "
